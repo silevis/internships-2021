ae5af5a1134ce5bd7552697bce9e741d13c5e3f6
Patryk Eliasz <patryk.eliasz@silevis.com>
2021-07-14 15:05:44 +0200

    - added cra files + husky + linter

---
+++ src/setupTests.ts
@@ -0,0 +1,6 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// eslint-disable-next-line extra-rules/no-commented-out-code
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
