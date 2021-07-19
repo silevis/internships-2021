e28cf5129b4a94194b2afd77efa9445e72c6719d
Internship <internship@silevis.com>
2021-07-19 16:04:46 +0200

    make book info title display properly

--- src/views/BookInfoPage.tsx
+++ src/views/BookInfoPage.tsx
@@ -13,27 +13,29 @@ const BookInfoPage = (props: { id: string; }) => {
     description: 'updated and expanded this invaluable resource. Unlike any other thesaurus available, this popular guide offers a wealth of natural language options in a convenient, A-to-Z format. It\'s ideal for helping users find the appropriate word or words for computer searches in the humanities, social sciences, and business. The second edition has added more than 9,000 entries to the first edition\'s extensive list. Now, the Thesaurus contains almost 21,000 search entries! New or expanded areas include broader coverage of business terms and humanities-including arts literature, philosophy, religion, and music',
     id: props.id,
     imageLinks: [
-      'http://books.google.pl/books?id=zYw3sYFtz9kC&dq=search+terms&hl=&source=gbs_api',
+      'https://cdn-lubimyczytac.pl/upload/books/4810000/4810807/604880-352x500.jpg',
     ],
     title: 'The Contemporary Thesaurus of Search Terms and Synonyms',
     year: new Date(),
   };
 
   return (
-    <div className="w-11/12 m-auto">
-      <div className="w-11/12 m-auto flex-row flex-wrap flex-auto">
+    <div className="w-11/12 m-auto mt-8">
+      <div className="w-11/12 m-auto flex flex-wrap items-start">
         {/* todo: image z powiekszeniem po najechaniu (osobny component)*/}
-        <img alt="ok" className="w-4/12" />
-        <div className="w-6/12">
-          <span>{book.title}</span> <br />
-          <span>subtitle</span>
-        </div>
-        <div className="w-2/12">
-          ocena
+        <img src={book.imageLinks[0]} alt="ok" className="w-1/3 max-h-56 object-scale-down" />
+        <div className="w-2/3 flex flex-wrap">
+          <div className="w-6/12 flex-auto">
+            <span className="text-xl">{book.title}</span> <br />
+            <span className="text-sm">subtitle</span>
+          </div>
+          <div className="w-2/12">
+            ocena
+          </div>
         </div>
       </div>
       <div className="w-4/5 m-auto">
-        szczegóły
+        {book.description}
       </div>
     </div>
   );
