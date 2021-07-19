05c7d5b56ea53d4188c3c580458a917bc701f1f0
SirBambi <m.bandura3@gmail.com>
2021-07-19 06:13:36 -0700

    Updated sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,15 +5,30 @@ function Sidebar() {
       lg:overflow-y-visible lg:pt-0 lg:w-60 xl:w-72 lg:block hidden shadow-sm dark:bg-gray-800"
     >
       <div className="items-center justify-between overflow-y-auto scrolling-touch">
-        <div className="border-b border-gray-400 mx-4 my-3 pl-2 text-l font-bold">
+        <div className="border-b border-gray-400 mx-4 my-5 pl-2 text-l font-bold">
           Filtry
         </div>
-        <nav className="mx-4 my-3 pl-4">
-          <ul className="list-disc">
-            <li>
-              <input type="text" className="shadow-xl" />
-            </li>
-          </ul>
+        <nav className="mx-4 my-3">
+          <div className="border-b border-gray-200 mx-1 pb-5 mt-5">
+            <label htmlFor="searchbar" className="mr-2">Search</label>
+            <input
+              type="text"
+              placeholder="Insert book name"
+              id="searchbar"
+              className="p-1 placeholder-gray-400 text-gray-600 border outline-none"
+            />
+          </div>
+          <div className="border-b border-gray-200 mx-1 pl-4 pb-5 mt-5">
+            <ul className="list-disc">
+              <li>Beletrystyka</li>
+              <li>Literatura faktu, publicystyka</li>
+              <li>Literatura popularnonaukowa</li>
+              <li>Literatura dziecięca</li>
+              <li>Komiksy</li>
+              <li>Poezja, dramat, satyra</li>
+              <li>Pozostałe</li>
+            </ul>
+          </div>
         </nav>
       </div>
     </div>
