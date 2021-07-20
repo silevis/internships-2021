2273b73d714fa67f526571d3c227176aadf57b29
Jakub Naporowski <jakub.naporowski2003@gmail.com>
2021-07-20 12:06:56 +0200

    Added login and register components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
-import useProfile from '../hooks/useProfile';
-import { IProfile } from '../interfaces/IProfile.interface';
-import supabase from '../utils/supabase';
-
 function App() {
-  // eslint-disable-next-line space-before-function-paren
-  const a = useProfile('1e7b2a5f-7423-4cc4-aa70-003db70da303');
-  // eslint-disable-next-line space-before-function-paren
-  const fetchCallback = useCallback(async () => {
-    // eslint-disable-next-line
-    const { user, error } = await supabase.auth.signUp({
-      email: 'somesone@email.com',
-      password: 'nhLLRVQzFHZGKbGhgAAU',
-    });
-    if (user) {
-      await supabase.from<IProfile>('profiles').insert({
-        id: user.id,
-        firstName: 'fr',
-        lastName: 'rfsd',
-      });
-    }
-  }, []);
-
-  useEffect(() => {
-    fetchCallback().then().catch();
-  }, [fetchCallback]);
-
   return (
     <div className="App">
       <header className="App-header">
