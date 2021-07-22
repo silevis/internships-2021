169ec9f2ec7c8be8f74065ba34a3572cd623cb68
PatrykSowinski <79578683+PatrykSowinski@users.noreply.github.com>
2021-07-22 10:33:44 -0700

    Test

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
 import { useUser, useUserUpdate } from './UserContext';
+import supabase from '../utils/supabase';
 
 function Navigation() {
   const globalUser = useUser();
@@ -17,6 +18,8 @@ function Navigation() {
       lastName: '',
     });
   };
+  console.log(supabase.auth.user());
+
 
   return (
     <nav className="bg-white shadow dark:bg-gray-800">
