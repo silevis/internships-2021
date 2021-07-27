5363662bcd8e60d81452724f26a1f43b139d0746
SirBambi <m.bandura3@gmail.com>
2021-07-27 03:42:31 -0700

    updated dropdown

--- src/components/UserDropdown.tsx
+++ src/components/UserDropdown.tsx
@@ -1,43 +1,44 @@
 import React, { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+interface DropdownItem {
+  link: string;
+  label: string;
+}
+
 interface UserDropdownProps {
   title: string | undefined;
+  items: DropdownItem[];
   logOut: () => void;
 }
 
-const UserDropdown: FC<UserDropdownProps> = ({ title, logOut }) => {
-  const [Open, setOpen] = useState(false);
+const UserDropdown: FC<UserDropdownProps> = ({ title, items, logOut }) => {
+  const [open, setOpen] = useState(false);
 
   return (
     <div>
       <div
         className="btn-nav-end"
-        onClick={() => setOpen(!Open)}
+        onClick={() => setOpen(!open)}
       >
         {title}
       </div>
-      {Open && (
+      {open && (
         <div
           className="dropdown block md:absolute w-auto overflow-hidden md:-translate-y-0
         bg-gray-700 md:bg-gray-600 text-white border border-t-0 border-gray-400 md:border-gray-800 rounded-sm shadow-inner md:shadow-none"
         >
-          <div className="menu-item border-b border-gray-400 md:border-gray-700">
-            <Link
-              to="/user"
-              className="cursor-pointer transition duration-400 ease-in-out hover:text-indigo-500"
-            >
-              User Profile
-            </Link>
-          </div>
-          <div className="menu-item border-b border-gray-400 md:border-gray-700">
-            <Link
-              to="/books-list"
-              className="cursor-pointer transition duration-400 ease-in-out hover:text-indigo-500"
-            >
-              User Books
-            </Link>
-          </div>
+          {items.map((item) => (
+            <div className="menu-item border-b border-gray-400 md:border-gray-700">
+              <Link
+                to={`${item.link}`}
+                className="cursor-pointer transition duration-400 ease-in-out hover:text-indigo-500"
+              >
+                {item.label}
+              </Link>
+            </div>
+          ))}
+
           <div className="menu-item">
             <div
               onClick={logOut}
