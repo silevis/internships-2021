2a0fff31aa78e1a1480c99e3a50c6834ce3df1f3
PatrykSowinski <79578683+PatrykSowinski@users.noreply.github.com>
2021-07-27 11:27:56 -0700

    Updated imports

--- src/interfaces/IPrivateRoute.interface.ts
+++ src/interfaces/IPrivateRoute.interface.ts
@@ -1,8 +1,6 @@
-import React from 'react';
 import { IProfile } from './IProfile.interface';
 
 export interface IPrivateRoute {
-  // component: React.FC;
   path: string;
   exact: boolean;
   user: IProfile | null;
