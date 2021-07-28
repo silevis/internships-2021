5dd196f87c2d27b33f37ee187d294088cb06aad0
Jakub Naporowski <jakub.naporowski2003@gmail.com>
2021-07-28 12:50:56 +0200

    Changed filter by rating

--- src/views/BookListView.tsx
+++ src/views/BookListView.tsx
@@ -19,14 +19,11 @@ const BookListView = () => {
     if (params?.q?.length < 1) params.q = '*';
     const getAllBooks = async () => {
       const q = `%${params.q ? params.q : '*'}%`;
-      let range = '.99';
-      if (rating === '0') range = '10';
       const { data: books } = await supabase
         .from<IBook>('books')
         .select('*')
         .ilike('title', q)
-        .gte('avgRating', rating)
-        .lte('avgRating', rating + range);
+        .gte('avgRating', rating);
       if (books !== null) {
         setData(books);
       }
