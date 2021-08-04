ecef5a272a72cbc3b200151ca676c265c12c4576
Jakub Naporowski <jakub.naporowski2003@gmail.com>
2021-08-04 12:54:48 +0200

    Added pagination to other views

--- src/views/UserBooksView.tsx
+++ src/views/UserBooksView.tsx
@@ -6,6 +6,7 @@ import { IBook } from '../interfaces/IBook.interface';
 import UserBook from '../components/booklists/UserBook';
 import NoResults from '../components/booklists/NoResults';
 import { PageExitAnimation } from '../components/App';
+import Pagination from '../components/booklists/Pagination';
 
 interface IBookProfileId {
   book: IBook;
@@ -15,6 +16,13 @@ interface IBookProfileId {
   profileId: string | undefined;
 }
 const UserBooksView = () => {
+  const [numberOfBooks, setNumberOfBooks] = useState<number | null>(10);
+  const [startIndex, setStartIndex] = useState<number>(0);
+  const [endIndex, setEndIndex] = useState<number>(9);
+  const onPageChange = (selectedPage: { selected: number }) => {
+    setStartIndex(selectedPage.selected * 10);
+    setEndIndex((selectedPage.selected * 10) + 9);
+  };
   const [info, setInfo] = useState<null | IBookProfileId[]>([]);
   const user = useUser();
 
@@ -22,15 +30,23 @@ const UserBooksView = () => {
     const { data } = await supabase
       .from<IBookProfileId>('borrowedBooks')
       .select('id, date, returnDate, book:books(*)')
-      .eq('profileId', user?.id);
-    if (data !== null) {
+      .eq('profileId', user?.id)
+      .range(startIndex, endIndex);
+      if (data !== null) {
       setInfo(data);
     }
+  }, [user, endIndex, startIndex]);
+  const getNumberOfBooks = useCallback(async () => {
+    const { count } = await supabase
+    .from<IBookProfileId>('borrowedBooks')
+    .select('id', { count: 'exact' })
+    .eq('profileId', user?.id);
+    setNumberOfBooks(count);
   }, [user]);
-
   useEffect(() => {
+    getNumberOfBooks();
     getBorrowedBooks();
-  }, [getBorrowedBooks]);
+  }, [getBorrowedBooks, getNumberOfBooks]);
 
   return (
     <motion.div exit={PageExitAnimation}>
@@ -46,6 +62,14 @@ const UserBooksView = () => {
           />
       ))}
         {info && info?.length < 1 && <NoResults />}
+      </div>
+      <div className="flex flex-row w-full justify-center mb-2 text-xl">
+        <Pagination
+          count={numberOfBooks || 10}
+          pageSize={10}
+          currentPage={startIndex / 10}
+          onPageChange={onPageChange}
+        />
       </div>
     </motion.div>
   );
